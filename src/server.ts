a622704232564f9092f34aaf2f23abce91725574
E-gy <balakhelisey@gmail.com>
2022-03-24 09:15:17 +0100

    log levell'd

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import log from 'loglevel';
-
+import prefix from 'loglevel-plugin-prefix';
+log.setLevel(process.env.LOGLEVEL as log.LogLevelDesc || log.levels.INFO);
+prefix.reg(log);
+prefix.apply(log, {
+	template: "[%t %l %n] ",
+	levelFormatter: (level) => level.toUpperCase(),
+	nameFormatter: (name) => name ?? "-",
+	timestampFormatter: (date) => date.toISOString(),
+});
 log.info("Rosa Back Central starting up!");
