de1a29ac72bbe4dfe198708a9ccac82532181dab
E-gy <balakhelisey@gmail.com>
2022-03-25 00:28:54 +0100

    db

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import type { Settings } from 'settings';
 import { Pool } from 'pg';
 
+export type Database = Pool;
+
 export default ((settings: Settings) => new Pool({
     max: 20,
     ssl: true,
