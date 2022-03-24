d40f9a7b1e1e7c60c14907365601b35f21ee1f71
E-gy <balakhelisey@gmail.com>
2022-03-24 12:43:21 +0100

    Settings proto functional

--- src/settings.ts
+++ src/settings.ts
@@ -1,28 +1,26 @@
-interface Settings {
-	db: string,
+export interface Settings {
+	db: {
+		connectionString?: string;
+		host?: string;
+		port?: number;
+		user?: string;
+		password?: string;
+		max?: number;
+	},
 }
 
 import fs from "fs";
 import yaml from "js-yaml";
 
-declare global {
-    namespace NodeJS {
-        interface Global {
-            settings: Settings|undefined;
-        }
-    }
-}
-global.settings = (() => {
-	if(typeof process.env.SETTINGS !== "undefined"){
-		try {
-			return yaml.load(fs.readFileSync(process.env.SETTINGS, "utf-8"));
-		} catch(e){
-			return yaml.load(process.env.SETTINGS);
-		}
-	} else try {
-		return yaml.load(fs.readFileSync("settings.yaml", "utf-8"));
+export default (() => {
+	if(typeof process.env.SETTINGS_FILE !== "undefined") try {
+		return yaml.load(fs.readFileSync(process.env.SETTINGS_FILE, "utf-8")) as Settings;
+	} catch(e){
+		return undefined;
+	} else if(typeof process.env.SETTINGS !== "undefined") return yaml.load(process.env.SETTINGS) as Settings;
+	else try {
+		return yaml.load(fs.readFileSync("settings.yaml", "utf-8")) as Settings;
 	} catch(e){
 		return undefined;
 	}
-})() as any;
-export default global.settings;
+});
