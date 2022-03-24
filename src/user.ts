117f7934c467ee898169763fc2cba502656be558
E-gy <balakhelisey@gmail.com>
2022-03-25 00:29:10 +0100

    user ops

--- src/user.ts
+++ src/user.ts
@@ -1,3 +1,4 @@
+import { Database } from "db";
 import { UUID } from "primitives";
 
 /**
@@ -10,6 +11,29 @@ export interface User {
     //TODO profile
 }
 
+/**
+ * User creation data
+ */
+export interface UserCreate {
+    name: string;
+}
+
+export async function getUser(db: Database, id: UUID): Promise<User> {
+    return (await db.query(`select * from users where id = :1`, [id])).rows[0];
+}
+
+export async function deleteUser(db: Database, id: UUID): Promise<void> {
+    await db.query(`delete * from users where id = :1`, [id]);
+}
+
+export async function createUser(db: Database, crea: UserCreate): Promise<UUID> {
+    const res = await db.query(`INSERT INTO users ('name') VALUES (:1)`, [crea.name]);
+    return res.rows[0].id;
+}
+
+/**
+ * User story
+ */
 export interface Story {
     id: UUID;
     user_id: UUID;
