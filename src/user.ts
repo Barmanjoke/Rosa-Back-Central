c05dc812ae77a9fa0245117efb113b54ad062e73
E-gy <balakhelisey@gmail.com>
2022-03-25 00:54:25 +0100

    fixed user ops

--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,6 @@
 import { Database } from "db";
 import { UUID } from "primitives";
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * User
@@ -16,19 +17,22 @@ export interface User {
  */
 export interface UserCreate {
     name: string;
+    temporary?: boolean;
 }
 
 export async function getUser(db: Database, id: UUID): Promise<User> {
-    return (await db.query(`select * from users where id = :1`, [id])).rows[0];
+    return (await db.query(`select * from users where id = $1`, [id])).rows[0];
 }
 
 export async function deleteUser(db: Database, id: UUID): Promise<void> {
-    await db.query(`delete * from users where id = :1`, [id]);
+    await db.query(`delete * from users where id = $1`, [id]);
 }
 
 export async function createUser(db: Database, crea: UserCreate): Promise<UUID> {
-    const res = await db.query(`INSERT INTO users ('name') VALUES (:1)`, [crea.name]);
-    return res.rows[0].id;
+    const uid = uuidv4();
+    const now = new Date();
+    await db.query(`INSERT INTO users (id, created_on, delete_on, name) VALUES ($1, $2, $3, $4)`, [uid, now, crea.temporary ? new Date(now.getFullYear(), now.getMonth(), now.getDay() + 60) : null, crea.name]);
+    return uid;
 }
 
 /**
